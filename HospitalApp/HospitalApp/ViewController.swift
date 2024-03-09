f857c3cad916cba1ba4ab4ca5dc1e6a0d1793075
sushant <sushanttoraskar16@gmail.com>
2024-03-09 17:02:01 +0530

    project created

---
+++ HospitalApp/HospitalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HospitalApp
+//
+//  Created by sushant's Macbook on 09/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
